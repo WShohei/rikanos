e4fbda3334d972f895db0093429de119b60fe999
WShohei <komachi221118@icloud.com>
2024-05-25 18:38:11 +0900

    fix link settings

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use core::arch::asm;
+use core::panic::PanicInfo;
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn kernel_main() -> () {
+    unsafe {
+        asm!("hlt");
+    }
 }
